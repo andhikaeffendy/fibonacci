aa984c7ae2bb9f118b9f4395a50fd981491bca09
Andhika Effendy <andhikaeffendy14@gmail.com>
2017-04-08 12:00:46 +0700

    Add files via upload

--- Nomor1A.java
+++ Nomor1A.java
@@ -22,11 +22,11 @@ public class Nomor1A {
         int n = show.nextInt();
         long fib[] = new long[n];
          
-        fib[0] = 0;
+        fib[0] = 1;
         fib[1] = 1;
          
         for(int i = 2; i < n; i++) {
-            fib[i] = fib[i-1] + fib[i-2];
+            fib[i] = fib[i-2] + fib[i-1];
         }
          
         for (int i = 0; i < n; i++) {
